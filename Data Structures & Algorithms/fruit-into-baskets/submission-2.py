1e91d665c0ba30a7c2f45346eb6b8670a825f12c
aathin2001 <113782721+aathin2001@users.noreply.github.com>
2026-04-28 18:52:42 +0530

    Add: fruit-into-baskets - submission-2

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+
+        basket = {}
+
+        l=r=res=0
+
+        while r<len(fruits):
+            basket[fruits[r]]=basket.get(fruits[r],0)+1
+
+            while len(basket)>2:
+                basket[fruits[l]]-=1
+                if basket[fruits[l]] == 0:
+                    basket.pop(fruits[l])
+                l+=1
+
+            res=max(res,sum(basket.values()))
+
+            r+=1
+
+        return res
+
+        
